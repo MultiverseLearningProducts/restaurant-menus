56429514568475184bca41f4ada67d289f193624
Dustin Alandzes <dustin.alandzes@hp.com>
2025-03-05 15:22:10 -0600

    Finish tests

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,14 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Model, DataTypes} = require('sequelize');
 
 // TODO - create a Menu model
+class Menu extends Model {}
 
+Menu.init({
+    title: DataTypes.STRING
+},
+    {
+        sequelize,
+        modelName: 'Menu',
+    })
 module.exports = {Menu};
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,19 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 
 // TODO - create a Restaurant model
+class Restaurant extends Model {}
+
+Restaurant.init(
+    {
+        name: DataTypes.STRING,
+        location: DataTypes.STRING,
+        cuisine: DataTypes.STRING,
+    },
+    {
+        sequelize,
+        modelName: 'Restaurant',
+    }
+)
 
 module.exports = {Restaurant};
